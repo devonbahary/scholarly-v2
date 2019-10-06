36cedb565b7783b6537c7c82149b095a07d409e8
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-05 20:08:19 -0700

    adds components/quotes/Quotes.js

---
+++ src/components/quotes/Quotes.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import styles from "../../styles/cards.scss";
+import CollectionIcon from "../icons/CollectionIcon";
+
+const Quote = ({ text, collectionTitle }) => (
+    <div className={styles.card}>
+        <div className={styles.cardBody}>
+            {text}
+        </div>
+        <div className={styles.cardFooter}>
+            <CollectionIcon /> {collectionTitle}
+        </div>
+    </div>
+);
+
+class Quotes extends Component {
+    state = {
+        quotes: [],
+    };
+
+    async componentDidMount() {
+        const response = await axios.get('/api/quotes');
+        this.setState(({
+            quotes: response.data,
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.quotes.map(quote => (
+                    <Quote key={quote.id} {...quote} />
+                ))}
+            </div>
+        );
+    };
+};
+
+export default Quotes;
