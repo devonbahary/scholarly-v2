736ae26699694ad9c10e00c59ba853d3a9ad6781
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-05 17:31:13 -0700

    adds body-parser middleware

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ import express from "express";
 import "./config";
 import "./databases/mongo-connect";
 import collections from "./routes/collections";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use('/api/collections', collections);
 
