f0344045ae135c37e4e42ee8271e64bd7892ee71
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-06 11:38:03 -0700

    adds <View />, a wrapper component that accepts header and body props and applies uniform styling to them

---
+++ src/components/View.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "../styles/views.scss";
+
+const View = ({ body, header }) => (
+    <div className={styles.view}>
+        <div className={styles.header}>
+            {header}
+        </div>
+        <div className={styles.body}>
+            {body}
+        </div>
+    </div>
+);
+
+export default View;
