61d82137a0bad7df9300e74987a51fe9456e0989
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-05 20:04:21 -0700

    adds /collections/quotes/:id

--- routes/collections.js
+++ routes/collections.js
@@ -1,5 +1,6 @@
 import express from "express";
 import CollectionsRepository from "../repositories/CollectionsRepository";
+import QuotesRepository from "../repositories/QuotesRepository";
 import ApiUtil from "../ApiUtil";
 
 const router = express.Router();
@@ -26,6 +27,16 @@ router.post('/', async (req, res) => {
         ApiUtil.errorResponse(res, err);
     }
 });
+
+router.get('/quotes/:id', async (req, res) => {
+    const quotesRepository = new QuotesRepository();
+    const collectionId = req.params.id;
+    try {
+        const results = await quotesRepository.getByCollectionId(collectionId);
+        res.send(results);
+    } catch (err) {
+        ApiUtil.errorResponse(res, err);
+    }
 });
 
 export default router;
