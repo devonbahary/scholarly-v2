cb2a3a8a82257467ecb685e92ed141f34c3e0a89
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-05 20:07:04 -0700

    adds components/icons/CollectionIcon.js and QuoteIcon.js

---
+++ src/components/icons/CollectionIcon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CollectionIcon = () => (
+    <i className="fas fa-book"></i>
+);
+
+export default CollectionIcon;
---
+++ src/components/icons/QuoteIcon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuoteIcon = () => (
+    <i className="fas fa-quote-right"></i>
+);
+
+export default QuoteIcon;
