d54a2836ed59a2908e2b0a86f6ebbe2cb3124794
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-05 20:08:31 -0700

    rework to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Collections from "./components/Collections";
+import { BrowserRouter as Router, NavLink, Route, Switch } from "react-router-dom";
+import Collections from "./components/collections/Collections";
+import Quotes from "./components/quotes/Quotes";
+import CollectionIcon from "./components/icons/CollectionIcon";
+import QuoteIcon from "./components/icons/QuoteIcon";
 
-class App extends Component {
-    render() {
-        return (
-            <Router>
+import styles from "./styles/App.scss";
+
+const Tab = props => (
+    <NavLink
+        exact={props.exact}
+        to={props.to}
+        className={styles.navLink}
+        activeClassName={styles.navLinkActive}
+    >
+        {props.children}
+    </NavLink>
+);
+
+const App = () => {
+    const routes = [{
+        path: "/collections",
+        component: Collections,
+        tabIcon: <CollectionIcon />,
+    }, {
+        path: "/quotes",
+        component: Quotes,
+        tabIcon: <QuoteIcon />,
+    }];
+
+    return (
+        <Router>
+            <main className={styles.main}>
                 <Switch>
-                    <Route path="/" component={Collections} />
+                    {routes.map(({ component, path }) => (
+                        <Route key={path} path={path} component={component} />
+                    ))}
                 </Switch>
-            </Router>
-        );
-    }
-}
+            </main>
+            <footer className={styles.footer}>
+                {routes.map(({ path, tabIcon }) => (
+                    <Tab key={path} to={path}>
+                        {tabIcon}
+                    </Tab>
+                ))}
+            </footer>
+        </Router>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
