a9a1d055ab924ac770ade242b381eaed8faf64ec
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-05 20:48:16 -0700

    adds <Home /> to App.js routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, NavLink, Route, Switch } from "react-router-dom";
 import Collections from "./components/collections/Collections";
+import Home from "./components/Home";
 import Quotes from "./components/quotes/Quotes";
 import CollectionIcon from "./components/icons/CollectionIcon";
 import QuoteIcon from "./components/icons/QuoteIcon";
@@ -21,6 +22,11 @@ const Tab = ({ children, exact, to}) => (
 
 const App = () => {
     const routes = [{
+        path: "/",
+        exact: true,
+        component: Home,
+        tabIcon: <i className="fas fa-home"></i>
+    }, {
         path: "/collections",
         component: Collections,
         tabIcon: <CollectionIcon />,
