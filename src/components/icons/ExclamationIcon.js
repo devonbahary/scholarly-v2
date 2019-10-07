fccc31c261d8a3256e55c46a1cdff4c61b4d4614
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-06 22:36:44 -0700

    adds <ExclamationIcon />

---
+++ src/components/icons/ExclamationIcon.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "../../styles/icons.scss";
+
+const ExclamationIcon = () => (
+    <i className={`fas fa-exclamation-triangle ${styles.error}`}></i>
+);
+
+export default ExclamationIcon;
