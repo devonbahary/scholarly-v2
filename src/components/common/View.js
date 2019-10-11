ca7152a0b673d62317b7cee8c93e838d4ce9d0bd
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-11 15:10:43 -0700

    adds <View /> and styles

---
+++ src/components/common/View.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "../../styles/View.scss";
+
+const View = ({ body, header }) => (
+    <section className={styles.view}>
+        <header className={styles.header}>
+            {header}
+        </header>
+        <div className={styles.body}>
+            {body}
+        </div>
+    </section>
+);
+
+export default View;
