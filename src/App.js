2115f769086a1ceee3bd53389126e66f00e04113
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-13 13:29:27 -0700

    implements <Collections />, <Home />, and <Quotes /> in App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, NavLink, Route, Switch } from "react-router-dom";
 
+import Collections from "./components/collections/Collections";
+import Home from "./components/Home";
+import Quotes from "./components/quotes/Quotes";
+
 import styles from "./styles/App.scss";
+
 
 const Tab = ({ children, exact, to}) => (
     <NavLink
+        activeClassName={styles.navLinkActive}
+        className={styles.navLink}
         exact={exact}
         to={to}
-        className={styles.navLink}
-        activeClassName={styles.navLinkActive}
     >
         {children}
     </NavLink>
 );
 
 const App = () => {
-    const routes = [];
+    const routes = [{
+        component: Home,
+        exact: true,
+        path: '/',
+        tabIcon: <i className="fas fa-home"></i>,
+    }, {
+        component: Collections,
+        path: '/collections',
+        tabIcon: <i className="fas fa-book"></i>,
+    }, {
+        component: Quotes,
+        path: '/quotes',
+        tabIcon: <i className="fas fa-quote-right"></i>,
+    }];
 
     return (
         <Router>
