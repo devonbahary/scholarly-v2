2c3778ffe3391306b2fb1d50f6901f856a8e1d20
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-18 09:26:02 -0700

    adds a headerButton prop to <View />

--- src/components/common/View.js
+++ src/components/common/View.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import styles from "./View.scss";
 
-const View = ({ body, headerIcon, headerTitle }) => (
+const View = ({ body, headerButton, headerIcon, headerTitle }) => (
     <section className={styles.view}>
         <header className={styles.header}>
             {headerIcon}
             <div className={styles.headerTitle}>
                 {headerTitle}
             </div>
+            {headerButton}
         </header>
         <div className={styles.body}>
             {body}
