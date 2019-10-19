fe8bdf8f3fb3e8c11a3777bbb19020fbc6b8c10e
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-19 15:14:43 -0700

    adds debouncedUpdateQuote(), addQuote(), createQuote(), resetAddingQuote(), and isAddingQuote() to Store.js

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
-import { action, observable } from "mobx";
+import { action, computed, observable } from "mobx";
 import Quote from "../models/Quote";
+
+const DEBOUNCE_UPDATE_QUOTE_MS = 200;
 
 export default class Store {
     @observable quotes = [];
@@ -11,7 +13,7 @@ export default class Store {
         this.quotesApi = quotesApi;
     };
 
-    debouncedUpdateQuote = _.debounce(quote => this.updateQuote(quote), 1000);
+    debouncedUpdateQuote = _.debounce(quote => this.updateQuote(quote), DEBOUNCE_UPDATE_QUOTE_MS);
 
     @action resetErrorQuoteId = () => {
         this.errorQuoteUIKey = null;
@@ -24,10 +26,21 @@ export default class Store {
         this.quotes = data.map(quote => new Quote(quote));
     };
 
+    @action addQuote = () => {
+        const quote = new Quote();
+        this.quotes = [ quote, ...this.quotes ];
+        this.setActiveQuote(quote);
+    };
+
+    @action createQuote = async quote => {
+        const { data } = await this.quotesApi.createQuote(quote);
+        quote.id = data.insertId;
+    };
+
     @action updateQuote = async quote => {
         this.resetErrorQuoteId();
         const success = await this.quotesApi.updateQuote(quote);
-        if (!success) this.errorQuoteId = quote.id;
+        if (!success) this.errorQuoteUIKey = quote.uiKey;
     };
 
     @action deleteQuote = async quote => {
@@ -43,6 +56,16 @@ export default class Store {
 
     @action resetActiveQuote = () => {
         this.activeQuoteUIKey = null;
+    };
+
+    @action resetAddingQuote = () => {
+        this.quotes.forEach(quote => {
+            if (!quote.id) quote.isDeleted = true;
+        });
+    };
+
+    @computed get isAddingQuote() {
+        return this.quotes.some(quote => !quote.id && !quote.isDeleted);
     };
 };
 
