017dca2d2ae77c8fa9b7e3473e3f35e3ce138964
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-20 16:10:09 -0700

    adds /models/Collection.js

---
+++ src/models/Collection.js
@@ -0,0 +1,23 @@
+import uuidv4 from "uuid/v4";
+import { computed, observable } from "mobx";
+
+export default class Collection {
+    @observable id;
+    @observable title = '';
+    @observable isDeleted = false;
+
+    uiKey = uuidv4();
+
+    constructor({ id = 0, quoteCount, title } = {}) {
+        this.id =  id;
+        this.quoteCount = quoteCount;
+        this.title = title;
+    };
+
+    @computed get asJSON() {
+        return {
+            id: this.id,
+            title: this.title,
+        };
+    };
+};
