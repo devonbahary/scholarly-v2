665e3f1c5a350a53fad3a6fb4af5a5f1313eac1c
Devon Bahary <devon.bahary@robinhealthcare.com>
2019-10-27 20:55:54 -0700

    updates to config.js for heroku ClearDB

--- server/config.js
+++ server/config.js
@@ -1,22 +1,22 @@
-let mongoUri;
-if (process.env.NODE_ENV === 'dev') {
-    mongoUri = 'mongodb://localhost:27017/scholarly';
-} else {
-    mongoUri = ''; // TODO
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-process.env.MONGO_URI = mongoUri;
-
+// let mongoUri;
+// if (process.env.NODE_ENV === 'dev') {
+//     mongoUri = 'mongodb://localhost:27017/scholarly';
+// } else {
+//     mongoUri = ''; // TODO
+// }
+// process.env.MONGO_URI = mongoUri;
 
 export default {
     mongo: {
         database: 'scholarly',
-        mongoUri,
+        mongoUri: '',
     },
     mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'scholarly',
+        host: isProd ? process.env.CLEARDB_DATABASE_URL : 'localhost',
+        user: isProd ? 'b35ab5cbb5c3d3' : 'root',
+        password: isProd ? '957e1c98' : '',
+        database: isProd ? 'heroku_23777d05b1260c2' : 'scholarly',
     },
 }
